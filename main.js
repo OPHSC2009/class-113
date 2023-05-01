87bc5ac4908133d007edf153fd7ff39b39b783c7
OPHSC2009 <92368269+OPHSC2009@users.noreply.github.com>
2023-05-01 23:12:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(500,500)
+    canvas.position(250,150)
+    video=createCapture(VIDEO)
+    video.hide()
+    tint_color="";
+
+
+
+}
+
+function draw(){
+    image (video,0,0,500,500)
+tint(tint_color)
+
+}
+
+function filter_tint(){
+tint_color=document.getElementById("color_name").value;
+
+
+
+}
+function take_snapshot(){
+save ("Snapshot.png")
+}
